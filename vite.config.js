08bcf5771defb526b70b3fbd4648f3f4dce2245e
Hedi-98 <mohamedhedi.kefi@esprit.tn>
2024-12-03 15:40:41 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// const path = require('path')
+import {
+  defineConfig
+} from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // root:path.resolve(__dirname, 'src'),
-  // build:{
-  //   outDir:"../dist"
-  // }
+  define: {
+    'process.env': {}
+  },
+  base: "/matias-dark/"
 })
