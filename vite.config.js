75bd9c8e0eb7f6f4b9d1c9c2a17c1f82a009adf3
Hedi-98 <mohamedhedi.kefi@esprit.tn>
2024-12-03 16:56:14 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      // https://rollupjs.org/configuration-options/
-    },
-  },
+  plugins: [react()],
+  base: '/matias-dark/',
 })
