921038c90c48a8d2b4965ef6255e3499a0907d95
Hedi-98 <mohamedhedi.kefi@esprit.tn>
2024-12-03 16:40:58 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import {
-  defineConfig
-} from 'vite'
+import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': {}
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
   },
-  base: "/matias-dark/"
 })
